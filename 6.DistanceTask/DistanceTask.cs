8b786973aee752f58a3d8ab540da77905b12ed2b
MrFreuden <mrfreuden@gmail.com>
2024-05-19 20:21:27 +0200

    6 task done

--- 6.DistanceTask/DistanceTask.cs
+++ 6.DistanceTask/DistanceTask.cs
@@ -4,9 +4,45 @@ namespace DistanceTask;
 
 public static class DistanceTask
 {
-    // Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
     public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
     {
-        return 0.0;
+        var ABLengthSquared = GetSquaredLength(ax, ay, bx, by);
+        if (ABLengthSquared == 0)
+            return GetDistance(ax, ay, x, y);
+
+        var t = GetProjection(ax, ay, bx, by, x, y, ABLengthSquared);
+
+        if (t < 0)
+            return GetDistance(ax, ay, x, y);
+        else if (t > 1)
+            return GetDistance(bx, by, x, y);
+
+        var Dx = ax + (bx - ax) * t;
+        var Dy = ay + (by - ay) * t;
+
+        return GetDistance(Dx, Dy, x, y);
+    }
+
+    private static double GetSquaredLength(double x1, double y1, double x2, double y2)
+    {
+        var dx = x2 - x1;
+        var dy = y2 - y1;
+        return dx * dx + dy * dy;
+    }
+
+    private static double GetDistance(double x1, double y1, double x2, double y2)
+    {
+        return Math.Sqrt(GetSquaredLength(x1, y1, x2, y2));
+    }
+
+    private static double GetProjection(double ax, double ay, double bx, double by, double x, double y, 
+        double ABLengthSquared)
+    {
+        var APx = x - ax;
+        var APy = y - ay;
+        var ABx = bx - ax;
+        var ABy = by - ay;
+        var dotProduct = APx * ABx + APy * ABy;
+        return dotProduct / ABLengthSquared;
     }
 }
