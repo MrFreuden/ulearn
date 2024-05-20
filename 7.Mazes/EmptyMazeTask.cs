a640420270bc626d82e8f66318a8ce5faa6d3811
MrFreuden <mrfreuden@gmail.com>
2024-05-20 06:34:02 +0200

    EmptyMazeTask done

---
+++ 7.Mazes/EmptyMazeTask.cs
@@ -0,0 +1,19 @@
+﻿namespace Mazes;
+
+public static class EmptyMazeTask
+{
+	public static void MoveOut(Robot robot, int width, int height)
+	{
+		while (robot.X != width - 2 || robot.Y != height - 2)
+		{
+            if (width - robot.X > height - robot.Y)
+				robot.MoveTo(Direction.Right);
+			else
+				robot.MoveTo(Direction.Down);
+		}
+		while (robot.X != width - 2)
+            robot.MoveTo(Direction.Right);
+		while (robot.Y != height - 2)
+			robot.MoveTo(Direction.Down);
+    }
+}
