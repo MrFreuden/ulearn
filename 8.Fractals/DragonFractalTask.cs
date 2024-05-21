9caf012d5cbdce814035f6a5d8f0728c5d1676d8
MrFreuden <mrfreuden@gmail.com>
2024-05-21 03:17:29 +0200

    fractal task done

--- 8.Fractals/DragonFractalTask.cs
+++ 8.Fractals/DragonFractalTask.cs
@@ -6,24 +6,33 @@ internal static class DragonFractalTask
 {
     public static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
     {
-        /*
-        Начните с точки (1, 0)
-        Создайте генератор рандомных чисел с сидом seed
-        
-        На каждой итерации:
+        double x = 1, y = 0;
+        var random = new Random(seed);
 
-        1. Выберите случайно одно из следующих преобразований и примените его к текущей точке:
+        for (int i = 0; i < iterationsCount; i++)
+        {
+            double angleInDegrees = random.Next() % 2 == 0 ? 45 : 135;
+            double angleInRadians = ToRadians(angleInDegrees);
+            (x, y) = Transform(x, y, angleInDegrees, angleInRadians);
+            pixels.SetPixel(x, y);
+        }
+    }
 
-            Преобразование 1. (поворот на 45° и сжатие в sqrt(2) раз):
-            x' = (x · cos(45°) - y · sin(45°)) / sqrt(2)
-            y' = (x · sin(45°) + y · cos(45°)) / sqrt(2)
+    private static double ToRadians(double angleInDegrees)
+    {
+        return Math.PI * angleInDegrees / 180.0;
+    }
 
-            Преобразование 2. (поворот на 135°, сжатие в sqrt(2) раз, сдвиг по X на единицу):
-            x' = (x · cos(135°) - y · sin(135°)) / sqrt(2) + 1
-            y' = (x · sin(135°) + y · cos(135°)) / sqrt(2)
-    
-        2. Нарисуйте текущую точку методом pixels.SetPixel(x, y)
+    private static (double, double) Transform(double x, double y, double angleInDegrees, double angleInRadians)
+    {
+        double newX = (x * Math.Cos(angleInRadians) - y * Math.Sin(angleInRadians)) / Math.Sqrt(2);
+        double newY = (x * Math.Sin(angleInRadians) + y * Math.Cos(angleInRadians)) / Math.Sqrt(2);
 
-        */
+        if (angleInDegrees == 135)
+        {
+            newX += 1;
+        }
+
+        return (newX, newY);
     }
 }
