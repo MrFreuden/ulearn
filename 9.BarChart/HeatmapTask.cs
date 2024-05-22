e67e6853e9d9816c1f1f6bb46b362aa74f831f26
MrFreuden <mrfreuden@gmail.com>
2024-05-22 09:28:59 +0200

    heatmap task done

--- 9.BarChart/HeatmapTask.cs
+++ 9.BarChart/HeatmapTask.cs
@@ -1,13 +1,32 @@
-﻿namespace Names;
+﻿using System.Xml.Linq;
+
+namespace Names;
 
 internal static class HeatmapTask
 {
     public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
     {
+        var months = new string[12];
+        var days = new string[30];
+        var map = new double[days.Length, months.Length];
+
+        for (int i = 0; i < months.Length; i++)
+            months[i] = (i + 1).ToString();
+        
+        for (int i = 0; i < days.Length; i++)
+            days[i] = (i + 2).ToString();
+
+        foreach (var nameData in names)
+        {
+            if (nameData.BirthDate.Day == 1)
+                continue;
+            map[nameData.BirthDate.Day - 2, nameData.BirthDate.Month - 1]++;
+        }
+
         return new HeatmapData(
             "Пример карты интенсивностей",
-            new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-            new[] { "a", "b", "c", "d" }, 
-            new[] { "X", "Y", "Z" });
+            map, 
+            days, 
+            months);
     }
 }
