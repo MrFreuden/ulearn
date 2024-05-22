a8344e6e82431c1a186c9e91457533adb58cbcd1
MrFreuden <mrfreuden@gmail.com>
2024-05-22 08:43:26 +0200

    GetBirthsPerDayHistogram done

---
+++ 9.BarChart/HistogramTask.cs
@@ -0,0 +1,21 @@
+﻿namespace Names;
+
+internal static class HistogramTask
+{
+    public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+    {
+        var values = new double[31];
+        var days = new string[31];
+        for (int i = 0; i < days.Length; i++)
+            days[i] = (i + 1).ToString();
+        foreach (var nameData in names)
+        {
+            if (nameData.Name == name && nameData.BirthDate.Day != 1)
+                values[nameData.BirthDate.Day - 1]++;
+        }
+        return new HistogramData(
+            $"Рождаемость людей с именем '{name}'", 
+            days, 
+            values);
+    }
+}
