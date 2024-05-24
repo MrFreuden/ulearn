09ed4705ee5ef3e82bb44c71e83cd68c4f90ee3a
MrFreuden <mrfreuden@gmail.com>
2024-05-24 04:45:39 +0200

    SentencesParser done

---
+++ 10.TextAnalysis/SentencesParserTask.cs
@@ -0,0 +1,26 @@
+﻿namespace TextAnalysis;
+
+static class SentencesParserTask
+{
+    public static List<List<string>> ParseSentences(string text)
+    {
+        var sentecnesSeparator = new char[] { '.', '!', '?', ';', ':', '(', ')'};
+        var wordsSeparator = new char[] {
+                                            '^', '#', '$', '-', '—', '+', '0', '1',
+                                            '2', '3', '4', '5', '6', '7', '8', '9', '=',
+                                            '\t', '\n', '\r', ',', '…', '“',
+                                            '”', '<', '>', '‘', '*', ' ', '/', '\u00A0'};
+        var sentencesList = new List<List<string>>();
+        var sentences = text.ToLower().Split(sentecnesSeparator, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var sentence in sentences)
+        {
+            var words = sentence.Split(wordsSeparator, StringSplitOptions.RemoveEmptyEntries);
+            if (words.Length > 0)
+            {
+                sentencesList.Add(words.ToList());
+            }
+        }
+        return sentencesList;
+    }
+}
