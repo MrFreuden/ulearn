4e9ae3a60caf29b4c19b87b20ab205c726b60a39
MrFreuden <mrfreuden@gmail.com>
2024-05-24 05:14:25 +0200

    sp fix

--- 10.TextAnalysis/SentencesParserTask.cs
+++ 10.TextAnalysis/SentencesParserTask.cs
@@ -1,21 +1,23 @@
-﻿namespace TextAnalysis;
+﻿using System;
+
+namespace TextAnalysis;
 
 static class SentencesParserTask
 {
     public static List<List<string>> ParseSentences(string text)
     {
-        var sentecnesSeparator = new char[] { '.', '!', '?', ';', ':', '(', ')'};
-        var wordsSeparator = new char[] {
+        var sentecneSeparators = new char[] { '.', '!', '?', ';', ':', '(', ')'};
+        var wordSeparators = new char[] {
                                             '^', '#', '$', '-', '—', '+', '0', '1',
                                             '2', '3', '4', '5', '6', '7', '8', '9', '=',
                                             '\t', '\n', '\r', ',', '…', '“',
                                             '”', '<', '>', '‘', '*', ' ', '/', '\u00A0'};
         var sentencesList = new List<List<string>>();
-        var sentences = text.ToLower().Split(sentecnesSeparator, StringSplitOptions.RemoveEmptyEntries);
+        var sentences = text.ToLowerInvariant().Split(sentecneSeparators, StringSplitOptions.RemoveEmptyEntries);
 
         foreach (var sentence in sentences)
         {
-            var words = sentence.Split(wordsSeparator, StringSplitOptions.RemoveEmptyEntries);
+            var words = sentence.Split(wordSeparators, StringSplitOptions.RemoveEmptyEntries);
             if (words.Length > 0)
             {
                 sentencesList.Add(words.ToList());
