14454690ecd6ce9848c65ad5fad7cf4467266c4e
MrFreuden <mrfreuden@gmail.com>
2024-05-28 12:11:55 +0200

    12 Grayscale done

--- 12.ImageFilter/GrayscaleTask.cs
+++ 12.ImageFilter/GrayscaleTask.cs
@@ -2,24 +2,23 @@
 
 public static class GrayscaleTask
 {
-	/* 
-	 * Переведите изображение в серую гамму.
-	 * 
-	 * original[x, y] - массив пикселей с координатами x, y. 
-	 * Каждый канал R,G,B лежит в диапазоне от 0 до 255.
-	 * 
-	 * Получившийся массив должен иметь те же размеры, 
-	 * grayscale[x, y] - яркость пикселя (x,y) в диапазоне от 0.0 до 1.0
-	 *
-	 * Используйте формулу:
-	 * Яркость = (0.299*R + 0.587*G + 0.114*B) / 255
-	 * 
-	 * Почему формула именно такая — читайте в википедии 
-	 * http://ru.wikipedia.org/wiki/Оттенки_серого
-	 */
-
 	public static double[,] ToGrayscale(Pixel[,] original)
 	{
-		return new double[original.GetLength(0), original.GetLength(1)];
+        var width = original.GetLength(0);
+        var height = original.GetLength(1);
+        var grayscale = new double[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                grayscale[x, y] = RGBToGrayscale(original[x, y].R, original[x, y].G, original[x, y].B);
+            }
+        }
+        return grayscale;
 	}
+
+    private static double RGBToGrayscale(double R, double G, double B)
+    {
+        return (0.299 * R + 0.587 * G + 0.114 * B) / 255;
+    }
 }
