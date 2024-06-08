e097ab4dae09c68d7d59bee277431adeab83820c
MrFreuden <mrfreuden@gmail.com>
2024-06-09 00:48:43 +0200

    Implement GetABAngle method and add tests

--- 16.Manipulator/TriangleTask.cs
+++ 16.Manipulator/TriangleTask.cs
@@ -10,7 +10,9 @@ public class TriangleTask
     /// </summary>
     public static double GetABAngle(double a, double b, double c)
     {
-        return 0.0;
+        if (a > 0 && b > 0 && c >= 0)
+            return Math.Acos((a * a + b * b - c * c) / (2 * a * b));
+        return double.NaN;
     }
 }
 
@@ -19,9 +21,12 @@ public class TriangleTask_Tests
 {
     [TestCase(3, 4, 5, Math.PI / 2)]
     [TestCase(1, 1, 1, Math.PI / 3)]
-    // добавьте ещё тестовых случаев!
+    [TestCase(2, 2, 2.8284271247461903, Math.PI / 2)]
+    [TestCase(5, 5, 5, Math.PI / 3)]
+    [TestCase(0, 0, 0, double.NaN)]
     public void TestGetABAngle(double a, double b, double c, double expectedAngle)
     {
-        Assert.Fail("Not implemented yet");
+        var actualAngle = TriangleTask.GetABAngle(a, b, c);
+        Assert.AreEqual(expectedAngle, actualAngle, 1e-6, "Angle is not calculated correctly");
     }
 }
