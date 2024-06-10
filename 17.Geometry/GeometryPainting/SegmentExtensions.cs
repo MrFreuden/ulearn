9faf2a324306ea5832bca482dea852b03dfca4ad
MrFreuden <mrfreuden@gmail.com>
2024-06-10 03:41:44 +0200

    SegmentExtensions implemented

---
+++ 17.Geometry/GeometryPainting/SegmentExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Media;
+using GeometryTasks;
+using System.Collections.Generic;
+
+namespace GeometryPainting;
+
+static class SegmentExtensions
+{
+    private static Dictionary<Segment, Color> colors = new();
+
+    public static Color GetColor(this Segment segment)
+    {
+        if (colors.TryGetValue(segment, out var val)) return val;
+        return Colors.Black;
+    }
+
+    public static void SetColor(this Segment segment, Color color)
+    {
+        colors[segment] = color;
+    }
+}
