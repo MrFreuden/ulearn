2599bafc5092c9075fc4c12d48bd8c10d7097067
MrFreuden <mrfreuden@gmail.com>
2024-06-10 03:13:49 +0200

    implemented basic methods in classes Segment, Geometry, Vector

--- 17.Geometry/Geometry/Geometry.cs
+++ 17.Geometry/Geometry/Geometry.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Geometry
+﻿namespace Geometry
 {
-    internal class Geometry
+    public class Geometry
     {
+        public static double GetLength(Vector vector)
+        {
+            return Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+        }
+
+        public static double GetLength(Segment segment)
+        {
+            return Math.Sqrt(Math.Pow(segment.End.X - segment.Begin.X, 2) + Math.Pow(segment.End.Y - segment.Begin.Y, 2));
+        }
+
+        public static Vector Add(Vector vector1, Vector vector2)
+        {
+            return new Vector { X = vector1.X + vector2.X, Y = vector1.Y + vector2.Y };
+        }
+
+        public static bool IsVectorInSegment(Vector vector, Segment segment)
+        {
+            var distance = Math.Sqrt(Math.Pow(segment.Begin.X - vector.X, 2) + Math.Pow(segment.Begin.Y - vector.Y, 2));
+            distance += Math.Sqrt(Math.Pow(segment.End.X - vector.X, 2) + Math.Pow(segment.End.Y - vector.Y, 2));
+            return distance == GetLength(segment);
+        } 
     }
 }
--- 17.Geometry/Geometry/Vector.cs
+++ 17.Geometry/Geometry/Vector.cs
@@ -2,6 +2,22 @@
 {
     public class Vector
     {
+        public double X;
+        public double Y;
 
+        public double GetLength()
+        {
+            return Geometry.GetLength(this);
+        }
+
+        public Vector Add(Vector vector)
+        {
+            return Geometry.Add(this, vector);
+        }
+
+        public bool Belongs(Segment segment)
+        {
+            return Geometry.IsVectorInSegment(this, segment);
+        }
     }
 }
