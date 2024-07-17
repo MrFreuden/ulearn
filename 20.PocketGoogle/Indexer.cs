0b04d241f697b92850be76cee1751337383b82be
MrFreuden <mrfreuden@gmail.com>
2024-07-17 03:43:47 +0200

    worked version with tests

--- 20.PocketGoogle/Indexer.cs
+++ 20.PocketGoogle/Indexer.cs
@@ -1,30 +1,114 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PocketGoogle;
-
-public class Indexer : IIndexer
+namespace PocketGoogle
 {
-	public void Add(int id, string documentText)
-	{
-		throw new NotImplementedException();
-	}
+    public class Indexer : IIndexer
+    {
+        private Dictionary<string, WordData> wordDictionary = new();
+        private char[] _delimeters = { ' ', '.', ',', '!', '?', ':', '-', '\r', '\n' };
+        public void Add(int id, string documentText)
+        {
+            var count = 0;
+            var text = documentText.Split(_delimeters);
+            foreach (var word in text)
+            {
+                if (string.IsNullOrEmpty(word))
+                {
+                }
+                else if (wordDictionary.TryGetValue(word, out WordData val))
+                {
+                    val.AddPositionAndId(id, count);
+                }
+                else
+                {
+                    wordDictionary.Add(word, new WordData(id, count));
+                }
+                count += word.Length;
+                count++;
+            }
+        }
 
-	public List<int> GetIds(string word)
-	{
-		throw new NotImplementedException();
-	}
+        public List<int> GetIds(string word)
+        {
+            if (wordDictionary.TryGetValue(word, out var wordData))
+            {
+                return wordData.GetIds();
+            }
+            return new List<int>();
+        }
 
-	public List<int> GetPositions(int id, string word)
-	{
-		throw new NotImplementedException();
-	}
+        public List<int> GetPositions(int id, string word)
+        {
+            if (wordDictionary.TryGetValue(word, out var wordData))
+            {
+                return wordData.GetPositions(id);
+            }
+            return new List<int>();
+        }
 
-	public void Remove(int id)
-	{
-		throw new NotImplementedException();
-	}
+        public void Remove(int id)
+        {
+            foreach (var item in wordDictionary)
+            {
+                if (item.Value.HatId(id))
+                {
+                    item.Value.Delete(id);
+
+                }
+                if (item.Value.GetIds().Count == 0)
+                {
+                    wordDictionary.Remove(item.Key);
+                }
+            }
+        }
+    }
+
+    public class WordData
+    {
+        private HashSet<int> _id = new();
+        //private List<int> _positions = new();
+        private Dictionary<int, List<int>> _idAndPositions = new();
+        public WordData(int id, int position)
+        {
+            _id.Add(id);
+           // _positions.Add(position);
+            _idAndPositions.Add(id, new List<int>() { position });
+        }
+
+        public void AddPositionAndId(int id, int position)
+        {
+            _id.Add(id);
+            //_positions.Add(position);
+            if (_idAndPositions.ContainsKey(id))
+            {
+                _idAndPositions[id].Add(position);
+            }
+            else
+            {
+                _idAndPositions.Add(id, new List<int>() { position });
+            }
+        }
+
+        public List<int> GetIds()
+        {
+            return _id.ToList();
+        }
+
+        public List<int> GetPositions(int id)
+        {
+            return _idAndPositions[id].ToList();
+        }
+
+        public bool HatId(int id)
+        {
+            return _id.Contains(id);
+        }
+
+        public void Delete(int id)
+        {
+            _id.Remove(id);
+            _idAndPositions.Remove(id);
+        }
+    }
 }
---
+++ 20.PocketGoogle/IndexerTests.cs
@@ -0,0 +1,131 @@
+﻿using NUnit.Framework;
+using PocketGoogle;
+using System.Linq;
+namespace PocketGoogleTests
+{
+    [TestFixture]
+    public class IndexerTests
+    {
+        private Indexer indexer;
+        [SetUp]
+        public void Setup()
+        {
+            indexer = new Indexer();
+        }
+
+        [Test]
+        [TestCase("A", 2, new int[] { 0, 4 })]
+        [TestCase("B", 0, new int[] { 2 })]
+        [TestCase("C", 0, new int[] { 4 })]
+        [TestCase("C", 2, new int[] { 2, 6 })]
+        [TestCase("F", 3, new int[] { 0 })]
+        public void GetPositions_ForEachLetter_ReturnsExpectedPositions(string word, int documentId, int[] expectedPositions)
+        {
+            // Arrange
+            indexer.Add(0, "A B C");
+            indexer.Add(1, "B C");
+            indexer.Add(2, "A C A C");
+            indexer.Add(3, "F, f ff");
+
+            // Act
+            var positions = indexer.GetPositions(documentId, word);
+
+            // Assert
+            Assert.AreEqual(expectedPositions, positions);
+        }
+
+        [Test]
+        public void Add_WhenCalled_AddsWordsWithCorrectPositions()
+        {
+            // Arrange
+            var indexer = new Indexer();
+            int documentId = 1;
+            string documentText = "Hello world!";
+
+            // Act
+            indexer.Add(documentId, documentText);
+
+            // Assert
+            var idsForHello = indexer.GetIds("Hello");
+            Assert.Contains(documentId, idsForHello);
+            var positionsForHello = indexer.GetPositions(documentId, "Hello");
+            Assert.AreEqual(0, positionsForHello.First());
+
+            var idsForWorld = indexer.GetIds("world");
+            Assert.Contains(documentId, idsForWorld);
+            var positionsForWorld = indexer.GetPositions(documentId, "world");
+            Assert.AreEqual(6, positionsForWorld.First());
+        }
+
+        [Test]
+        public void Add_WithMultipleDocuments_HandlesWordsCorrectly()
+        {
+            // Arrange
+            var indexer = new Indexer();
+            indexer.Add(1, "Hello world");
+            indexer.Add(2, "world peace");
+
+            // Assert
+            var ids = indexer.GetIds("world");
+            Assert.AreEqual(2, ids.Count);
+            Assert.IsTrue(ids.Contains(1) && ids.Contains(2));
+        }
+
+        [Test]
+        public void Add_WithRepeatingWordsInDocument_TracksAllPositionsCorrectly()
+        {
+            // Arrange
+            var indexer = new Indexer();
+            indexer.Add(1, "Hello Hello world");
+
+            // Assert
+            var positions = indexer.GetPositions(1, "Hello");
+            Assert.AreEqual(2, positions.Count);
+            Assert.IsTrue(positions.Contains(0) && positions.Contains(6));
+        }
+
+        [Test]
+        public void Remove_RemovesSingleDocumentById()
+        {
+            // Arrange
+            indexer.Add(1, "Hello world");
+            indexer.Add(2, "Goodbye world");
+
+            // Act
+            indexer.Remove(1);
+
+            // Assert
+            Assert.IsFalse(indexer.GetIds("Hello").Contains(1), "Document ID 1 should have been removed.");
+            Assert.IsTrue(indexer.GetIds("Goodbye").Contains(2), "Document ID 2 should still exist.");
+        }
+
+        [Test]
+        public void Remove_KeepsWordsFromOtherDocuments()
+        {
+            // Arrange
+            indexer.Add(1, "Shared word");
+            indexer.Add(2, "Shared word");
+
+            // Act
+            indexer.Remove(1);
+
+            // Assert
+            var idsForSharedWord = indexer.GetIds("Shared");
+            Assert.IsFalse(idsForSharedWord.Contains(1), "Document ID 1 should have been removed.");
+            Assert.IsTrue(idsForSharedWord.Contains(2), "Document ID 2 should still be associated with 'Shared'.");
+        }
+
+        [Test]
+        public void Remove_DoesNothingWhenIdDoesNotExist()
+        {
+            // Arrange
+            indexer.Add(1, "Existing document");
+
+            // Act
+            indexer.Remove(999); // Non-existent ID
+
+            // Assert
+            Assert.IsTrue(indexer.GetIds("Existing").Contains(1), "Document ID 1 should still exist.");
+        }
+    }
+}
