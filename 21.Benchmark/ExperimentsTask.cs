92e17f3a98eabb59b8c4f8db56778d847686e698
MrFreuden <mrfreuden@gmail.com>
2024-07-23 15:19:02 +0200

    Code Quality: Add Abstract fabric to create Experiments for Charts

--- 21.Benchmark/ExperimentsTask.cs
+++ 21.Benchmark/ExperimentsTask.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace StructBenchmarking;
@@ -7,41 +8,57 @@ public class Experiments
 	public static ChartData BuildChartDataForArrayCreation(
 		IBenchmark benchmark, int repetitionsCount)
 	{
-		var classesTimes = new List<ExperimentResult>();
-		var structuresTimes = new List<ExperimentResult>();
-
-        foreach (var fieldCount in Constants.FieldCounts)
-        {
-			classesTimes.Add(new ExperimentResult(fieldCount, benchmark.MeasureDurationInMs(new ClassArrayCreationTask(fieldCount), repetitionsCount)));
-			structuresTimes.Add(new ExperimentResult(fieldCount, benchmark.MeasureDurationInMs(new StructArrayCreationTask(fieldCount), repetitionsCount)));
-        }
-
         return new ChartData
-		{
-			Title = "Create array",
-			ClassPoints = classesTimes,
-			StructPoints = structuresTimes,
+        {
+            Title = "Create array",
+            ClassPoints = new ArrayCreationFabric().CreateClass(benchmark, repetitionsCount),
+			StructPoints = new ArrayCreationFabric().CreateStructure(benchmark, repetitionsCount),
 		};
 	}
 
 	public static ChartData BuildChartDataForMethodCall(
 		IBenchmark benchmark, int repetitionsCount)
 	{
-		var classesTimes = new List<ExperimentResult>();
-		var structuresTimes = new List<ExperimentResult>();
-
-
-		foreach (var fieldCount in Constants.FieldCounts)
-        {
-            classesTimes.Add(new ExperimentResult(fieldCount, benchmark.MeasureDurationInMs(new MethodCallWithClassArgumentTask(fieldCount), repetitionsCount)));
-            structuresTimes.Add(new ExperimentResult(fieldCount, benchmark.MeasureDurationInMs(new MethodCallWithStructArgumentTask(fieldCount), repetitionsCount)));
-        }
-
         return new ChartData
 		{
 			Title = "Call method with argument",
-			ClassPoints = classesTimes,
-			StructPoints = structuresTimes,
-		};
+            ClassPoints = new MethodCallFabric().CreateClass(benchmark, repetitionsCount),
+            StructPoints = new MethodCallFabric().CreateStructure(benchmark, repetitionsCount),
+        };
 	}
+}
+
+public abstract class ExperimentsFabric
+{
+    protected List<ExperimentResult> CreateExperiment(IBenchmark benchmark, int repetitionsCount, Func<int, ITask> func)
+    {
+        var res = new List<ExperimentResult>();
+        foreach (var fieldCount in Constants.FieldCounts)
+        {
+            res.Add(new ExperimentResult(fieldCount, benchmark.MeasureDurationInMs(func(fieldCount), repetitionsCount)));
+        }
+        return res;
+    }
+
+    public abstract List<ExperimentResult> CreateClass(IBenchmark benchmark, int repCount);
+
+    public abstract List<ExperimentResult> CreateStructure(IBenchmark benchmark, int repCount);
+}
+
+public class ArrayCreationFabric : ExperimentsFabric
+{
+    public override List<ExperimentResult> CreateClass(IBenchmark benchmark, int repCount)
+        => CreateExperiment(benchmark, repCount, fieldCount => new ClassArrayCreationTask(fieldCount));
+
+    public override List<ExperimentResult> CreateStructure(IBenchmark benchmark, int repCount)
+        => CreateExperiment(benchmark, repCount, fieldCount => new StructArrayCreationTask(fieldCount));
+}
+
+public class MethodCallFabric : ExperimentsFabric
+{
+    public override List<ExperimentResult> CreateClass(IBenchmark benchmark, int repCount)
+        => CreateExperiment(benchmark, repCount, fieldCount => new MethodCallWithClassArgumentTask(fieldCount));
+
+    public override List<ExperimentResult> CreateStructure(IBenchmark benchmark, int repCount)
+        => CreateExperiment(benchmark, repCount, fieldCount => new MethodCallWithStructArgumentTask(fieldCount));
 }
