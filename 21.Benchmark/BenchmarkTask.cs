5d1affe8c92fdf70ea51f3118f9168b7da0663fb
MrFreuden <mrfreuden@gmail.com>
2024-07-23 13:40:30 +0200

    Add MeasureDurationInMs and Tests

--- 21.Benchmark/BenchmarkTask.cs
+++ 21.Benchmark/BenchmarkTask.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 using NUnit.Framework;
 
 namespace StructBenchmarking;
@@ -8,20 +9,72 @@ public class Benchmark : IBenchmark
 {
     public double MeasureDurationInMs(ITask task, int repetitionCount)
     {
-        GC.Collect();                   // Эти две строчки нужны, чтобы уменьшить вероятность того,
-        GC.WaitForPendingFinalizers();  // что Garbadge Collector вызовется в середине измерений
-                                        // и как-то повлияет на них.
-           
-		throw new NotImplementedException();
+        if (repetitionCount <= 0)
+            throw new ArgumentException("Repetition count must be greater than zero.", nameof(repetitionCount));
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        
+        task.Run();
+        Stopwatch sw = Stopwatch.StartNew();
+        for (int i = 0; i < repetitionCount; i++)
+        {
+            task.Run();
+        }
+        sw.Stop();
+        return sw.Elapsed.TotalMilliseconds / repetitionCount;
 	}
 }
 
 [TestFixture]
 public class RealBenchmarkUsageSample
 {
+    public class StringConstructor : ITask
+    {
+        private int repetitionCount;
+
+        public StringConstructor(int repetitionCount)
+        {
+            this.repetitionCount = repetitionCount;
+        }
+
+        public void Run()
+        {
+            var stringConstructor = new string('a', repetitionCount);
+        }
+    }
+
+    public class StringBuilderConstructor : ITask
+    {
+        private int repetitionCount;
+
+        public StringBuilderConstructor(int repetitionCount)
+        {
+            this.repetitionCount = repetitionCount;
+        }
+
+        public void Run()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < repetitionCount; i++)
+            {
+                sb.Append('a');
+            }
+            var stringBuilder = sb.ToString();
+        }
+    }
+
     [Test]
     public void StringConstructorFasterThanStringBuilder()
     {
-        throw new NotImplementedException();
+        var repetitionCount = 10000;
+        var benchmark = new Benchmark();
+        var stringTask = new StringConstructor(repetitionCount);
+        var sbTask = new StringBuilderConstructor(repetitionCount);
+
+        var stringConstructorResult = benchmark.MeasureDurationInMs(stringTask, repetitionCount);
+        var sbConstructorResult = benchmark.MeasureDurationInMs(sbTask, repetitionCount);
+
+        Assert.Less(stringConstructorResult, sbConstructorResult);
     }
 }
