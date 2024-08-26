1e15f2214f0ef11ee34368c6b2815208dfeead1a
MrFreuden <mrfreuden@gmail.com>
2024-08-27 00:23:08 +0200

    Add my linked list class

--- 22.LimitedSizeStack/LimitedSizeStack.cs
+++ 22.LimitedSizeStack/LimitedSizeStack.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace LimitedSizeStack;
 
@@ -33,4 +34,117 @@ public class LimitedSizeStack<T>
     }
 
     public int Count => _values.Count;
+}
+
+public class MyLimitedSizeStack<T>
+{
+    private MyLinkedList<T> _values;
+    private readonly int _undoLimit;
+    public MyLimitedSizeStack(int undoLimit)
+    {
+        _values = new MyLinkedList<T>();
+        _undoLimit = undoLimit;
+    }
+
+    public void Push(T item)
+    {
+        if (_undoLimit == 0)
+        {
+            return;
+        }
+        if (Count == _undoLimit)
+        {
+            _values.RemoveFirst();
+        }
+        _values.AddLast(item);
+    }
+
+    public T Pop()
+    {
+        var val = _values.Last.Value;
+        _values.RemoveLast();
+        return val;
+    }
+
+    public int Count => _values.Count;
+}
+
+public class MyLinkedList<T>
+{
+    private ListItem<T>? _first;
+    private ListItem<T>? _last;
+    public ListItem<T>? First => _first;
+    public ListItem<T>? Last => _last;
+    private int _count;
+
+    public MyLinkedList()
+    {
+        _first = null;
+        _last = null;
+    }
+
+    public void AddFirst(T value)
+    {
+        var n = new ListItem<T> { Value = value, Next = _first };
+        if (_count != 0 && _first != null)
+        {
+            _first.Prev = n;
+        }
+        _first = n;
+        _count++;
+    }
+
+    public void AddLast(T value)
+    {
+        var n = new ListItem<T> { Value = value, Prev = _last };
+
+        if (_count != 0 && _last != null)
+        {
+            _last.Next = n;
+        }
+        _first ??= n;
+        _last = n;
+        _count++;
+    }
+
+    public void RemoveFirst()
+    {
+        if (_count == 0)
+        {
+            return;
+        }
+
+        var n = _first?.Next;
+        if (n != null)
+        {
+            n.Prev = null;
+        }
+        _first = n;
+        _count--;
+    }
+
+    public void RemoveLast()
+    {
+        if (_count == 0)
+        {
+            return;
+        }
+
+        var n = _last?.Prev;
+        if (n != null)
+        {
+            n.Next = null;
+        }
+        _last = n;
+        _count--;
+    }
+
+    public int Count => _count;
+}
+
+public class ListItem<T>
+{
+    public T Value { get; set; } = default!;
+    public ListItem<T>? Next { get; set; }
+    public ListItem<T>? Prev { get; set; }
 }
