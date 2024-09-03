ce523d948bfd5d9b24a6b6c1228e9a25eac0a00f
MrFreuden <mrfreuden@gmail.com>
2024-09-03 20:53:32 +0200

    MovingAverageTask done

--- 24.Smooth/MovingAverageTask.cs
+++ 24.Smooth/MovingAverageTask.cs
@@ -6,7 +6,20 @@ public static class MovingAverageTask
 {
 	public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
 	{
-		//Fix me!
-		return data;
+		Queue<double> window = new();
+		var sumValues = 0.0;
+        foreach (var item in data)
+		{
+            window.Enqueue(item.OriginalY);
+			sumValues += item.OriginalY;
+
+            if (window.Count > windowWidth)
+			{
+                sumValues -= window.Dequeue();
+			}
+		
+			var average = sumValues / window.Count;
+            yield return item.WithAvgSmoothedY(average);
+		}
 	}
 }
