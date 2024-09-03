677e0ee8ea17a8275f5584ff2a79b95aa7de588d
MrFreuden <mrfreuden@gmail.com>
2024-09-03 20:28:34 +0200

    ExpSmoothingTask done

--- 24.Smooth/ExpSmoothingTask.cs
+++ 24.Smooth/ExpSmoothingTask.cs
@@ -6,7 +6,19 @@ public static class ExpSmoothingTask
 {
 	public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
 	{
-		//Fix me!
-		return data;
+		double? previousSmoothedValue = null;
+
+        foreach (var item in data)
+		{
+			if (previousSmoothedValue == null)
+			{
+				previousSmoothedValue = item.OriginalY;
+			}
+			else
+			{
+                previousSmoothedValue = alpha * item.OriginalY + (1 - alpha) * previousSmoothedValue.Value;
+            }
+            yield return item.WithExpSmoothedY(previousSmoothedValue.Value);
+		}
 	}
 }
