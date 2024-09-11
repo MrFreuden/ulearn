940178a7bce59554a4bfa2ed3c484b8812b318f5
MrFreuden <mrfreuden@gmail.com>
2024-09-11 09:02:49 +0200

    task done

--- 28.Rocket/ControlTask.cs
+++ 28.Rocket/ControlTask.cs
@@ -1,9 +1,26 @@
+using System;
+
 namespace func_rocket;
 
 public class ControlTask
 {
-	public static Turn ControlRocket(Rocket rocket, Vector target)
-	{
-		return Turn.None;
-	}
+    private static double _totalAngle;
+    public static Turn ControlRocket(Rocket rocket, Vector target)
+    {
+        Vector distanceVector = target - rocket.Location;
+
+        if (Math.Abs(distanceVector.Angle - rocket.Direction) < 0.5
+                || Math.Abs(distanceVector.Angle - rocket.Velocity.Angle) < 0.5)
+        {
+            _totalAngle = (distanceVector.Angle - rocket.Direction + distanceVector.Angle - rocket.Velocity.Angle) / 2;
+        }
+        else
+        {
+            _totalAngle = distanceVector.Angle - rocket.Direction;
+        }
+
+        if (_totalAngle < 0)
+            return Turn.Left;
+        return _totalAngle > 0 ? Turn.Right : Turn.None;
+    }
 }
