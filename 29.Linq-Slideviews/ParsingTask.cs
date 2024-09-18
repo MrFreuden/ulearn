c4616d1bff0d918d0b509e309a8363dc2ee7b0ca
MrFreuden <mrfreuden@gmail.com>
2024-09-18 11:44:31 +0200

    Refactor ParseSlideRecords

--- 29.Linq-Slideviews/ParsingTask.cs
+++ 29.Linq-Slideviews/ParsingTask.cs
@@ -12,18 +12,25 @@ public class ParsingTask
 	/// <remarks>Метод должен пропускать некорректные строки, игнорируя их</remarks>
 	public static IDictionary<int, SlideRecord> ParseSlideRecords(IEnumerable<string> lines)
 	{
-		var s = lines
+        return lines
 			.Skip(1)
 			.Where(line => !string.IsNullOrEmpty(line) && char.IsDigit(line[0]))
 			.Select(line => line.Split(';'))
-			.Select(arr => arr
-				.Select((line, index) => index == 1 ? char.ToUpper(line[0]) + line.Substring(1) : line)
-				.ToArray())
-			.Where(arr => arr.Length == 3)
-			.Select(arr => new SlideRecord(int.Parse(arr[0]), (SlideType)Enum.Parse(typeof(SlideType), arr[1]), arr[2]))
-			.ToDictionary(g => g.SlideId, g => g);
-		;
-		return s;
+			.Select(arr =>
+			{
+				var id = int.Parse(arr[0]);
+				var typeString = char.ToUpper(arr[1][0]) + arr[1].Substring(1);
+
+                if (Enum.TryParse(typeString, out SlideType slideType))
+                {
+                    return new { Id = id, SlideType = slideType, Title = arr[2] };
+                }
+                else
+                {
+                    throw new ArgumentException($"Некорректное значение SlideType: {typeString}");
+                }
+            })
+            .ToDictionary(group => group.Id, group => new SlideRecord(group.Id, group.SlideType, group.Title));
 	}
 
 	/// <param name="lines">все строки файла, которые нужно распарсить. Первая строка — заголовочная.</param>
