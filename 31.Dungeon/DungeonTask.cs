260224d8b812d4a4e43daab471a1e342ab34443b
MrFreuden <mrfreuden@gmail.com>
2024-09-25 15:24:06 +0200

    refactoring

--- 31.Dungeon/DungeonTask.cs
+++ 31.Dungeon/DungeonTask.cs
@@ -7,53 +7,62 @@ public class DungeonTask
 {
     public static MoveDirection[] FindShortestPath(Map map)
     {
-        var chestsPoint = map.Chests.ToDictionary(chest => chest.Location, chest => chest);
-        
-        var startToChest = BfsTask.FindPaths(map, map.InitialPosition, chestsPoint.Values.ToArray());
+        var shortestPath = GetShortestPath(map);
 
+        if (shortestPath == null)
+        {
+            return FindPathWithoutChest(map);
+        }
+
+        var fullPath = shortestPath.Item1.Concat(shortestPath.Item2.Skip(1));
+
+        return GetDirectionsFromPath(fullPath);
+    }
+    
+    private static Tuple<IEnumerable<Point>, SinglyLinkedList<Point>, byte>? GetShortestPath(Map map)
+    {
+        var combinedPaths = GetCombinedPaths(map);
+        return combinedPaths
+            .OrderBy(path => path.Item1.Count() + path.Item2.Count())
+            .ThenByDescending(way => way.Item3)
+            .FirstOrDefault();
+    }
+
+    private static IEnumerable<Tuple<IEnumerable<Point>, SinglyLinkedList<Point>, byte>> GetCombinedPaths(Map map)
+    {
+        var chestsPoint = map.Chests.ToDictionary(chest => chest.Location, chest => chest);
+        var startToChest = BfsTask.FindPaths(map, map.InitialPosition, chestsPoint.Values.ToArray());
         var endToChest = BfsTask.FindPaths(map, map.Exit, chestsPoint.Values.ToArray());
 
         var combinedPaths = startToChest.Join(endToChest,
                             startPath => startPath.First(),
                             endPath => endPath.First(),
-                                (startPath, endPath) => new
-                                {
-                                    MidToStart = startPath.Reverse(),
-                                    MidToEnd = endPath,
-                                    ChestValue = chestsPoint[startPath.First()].Value
-                                });
+                                (startPath, endPath) => 
+                                Tuple.Create(startPath.Reverse(), endPath, chestsPoint[startPath.First()].Value));
 
-        var shortestPath = combinedPaths
-            .OrderBy(path => path.MidToStart.Count() + path.MidToEnd.Count())
-            .ThenByDescending(way => way.ChestValue)
-            .FirstOrDefault();
-
-        if (shortestPath == null || !startToChest.Any()|| !endToChest.Any())
-        {
-            return FindPathWithoutChest(map);
-        }
-
-        var fullPath = shortestPath.MidToStart.Concat(shortestPath.MidToEnd.Skip(1));
-
-        return GetDirectionsFromPath(fullPath);
-    }
-    private static MoveDirection[] GetDirectionsFromPath(IEnumerable<Point> path)
-    {
-        return path.Zip(path.Skip(1), (current, previous) => GetOffset(current, previous))
-            .ToArray();
+        return combinedPaths;
     }
 
     private static MoveDirection[] FindPathWithoutChest(Map map)
     {
         var directPath = BfsTask.FindPaths(map, map.Exit, new Chest[] { new Chest(map.InitialPosition, 0) })
                                 .FirstOrDefault();
-        if (directPath == null) return new MoveDirection[0];
+        if (directPath == null) return Array.Empty<MoveDirection>();
 
         return GetDirectionsFromPath(directPath);
     }
 
-    private static MoveDirection GetOffset(Point current, Point? previous)
+    private static MoveDirection[] GetDirectionsFromPath(IEnumerable<Point> path)
     {
+        return path
+            .Zip(path.Skip(1), (current, previous) => GetOffset(current, previous))
+            .ToArray();
+    }
+
+    private static MoveDirection GetOffset(Point current, Point previous)
+    {
+        if (current == null || previous == null)
+            throw new ArgumentNullException(nameof(current));
         return Walker.ConvertOffsetToDirection(new Point(previous.X - current.X, previous.Y - current.Y));
     }
 }
