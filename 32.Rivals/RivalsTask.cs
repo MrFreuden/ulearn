9fe8bb4fd649875091fc5de77fa2482415ae7764
MrFreuden <mrfreuden@gmail.com>
2024-09-26 22:24:28 +0200

    refactor

--- 32.Rivals/RivalsTask.cs
+++ 32.Rivals/RivalsTask.cs
@@ -6,19 +6,19 @@ namespace Rivals;
 
 public class RivalsTask
 {
-	public static IEnumerable<OwnedLocation> AssignOwners(Map map)
+    private readonly static List<Point> _possibleDirections = new()
+    {
+        new Point(0, -1),
+        new Point(0, 1),
+        new Point(-1, 0),
+        new Point(1, 0),
+    };
+    
+    public static IEnumerable<OwnedLocation> AssignOwners(Map map)
 	{
-        var i = 0;
         var visitedPoints = new HashSet<Point>();
-        var chestsPoint = map.Chests.ToDictionary(chest => chest);
-        var playersQueue = new Queue<(Point Player, int Index, Queue<Point> PlayerQueue)>(
-            map.Players
-            .Select(player => 
-            {
-                var queue = new Queue<Point>();
-                queue.Enqueue(player);
-                return (Player: player, Index: i++, PlayerQueue: queue);
-            } ));
+        var chestsPoint = map.Chests.ToHashSet();
+        var playersQueue = InitializePlayersQueue(map.Players);
 
         foreach (var player in playersQueue)
         {
@@ -29,25 +29,13 @@ public class RivalsTask
         while (playersQueue.Count != 0)
         {
             var currentPlayer = playersQueue.Dequeue();
+            var count = currentPlayer.PlayerQueue.Count;
 
-            var currentQ = new Queue<Point>(currentPlayer.PlayerQueue);
-            currentPlayer.PlayerQueue.Clear();
-
-            while (currentQ.Count != 0)
+            for (int i = 0; i < count; i++)
             {
-                var point = currentQ.Dequeue();
-                foreach (var nextPoint in GetNeighbors(map, point))
+                foreach (var ownedLocation in ProcessNeighbors(map, currentPlayer, visitedPoints, chestsPoint))
                 {
-                    if (visitedPoints.Contains(nextPoint)) continue;
-
-                    yield return new OwnedLocation(currentPlayer.Index, nextPoint,
-                        CalculateDistance(currentPlayer.Player.X, currentPlayer.Player.Y, nextPoint.X, nextPoint.Y));
-                    visitedPoints.Add(nextPoint);
-
-                    if (!chestsPoint.TryGetValue(nextPoint, out var chestPoint))
-                    {
-                        currentPlayer.PlayerQueue.Enqueue(nextPoint);
-                    }
+                    yield return ownedLocation;
                 }
             }
 
@@ -58,25 +46,52 @@ public class RivalsTask
         }
     }
 
-
-    private static int CalculateDistance(double x1, double y1, double x2, double y2)
+    private static Queue<(Point Player, int Index, Queue<Point> PlayerQueue)> InitializePlayersQueue(Point[] players)
     {
-        return (int)(Math.Abs(x2 - x1) + Math.Abs(y2 - y1));
+        return new Queue<(Point Player, int Index, Queue<Point> PlayerQueue)>(
+            players
+            .Select((player, index) =>
+            {
+                var queue = new Queue<Point>();
+                queue.Enqueue(player);
+                return (Player: player, Index: index, PlayerQueue: queue);
+            })
+            );
+    }
+
+    private static IEnumerable<OwnedLocation> ProcessNeighbors(
+        Map map,
+        (Point Player, int Index, Queue<Point> PlayerQueue) currentPlayer,
+        HashSet<Point> visitedPoints,
+        HashSet<Point> chestsPoint)
+    {
+        var point = currentPlayer.PlayerQueue.Dequeue();
+        foreach (var nextPoint in GetNeighbors(map, point))
+        {
+            if (visitedPoints.Contains(nextPoint)) continue;
+
+            yield return new OwnedLocation(currentPlayer.Index, nextPoint,
+                CalculateDistance(currentPlayer.Player.X, currentPlayer.Player.Y, nextPoint.X, nextPoint.Y));
+
+            visitedPoints.Add(nextPoint);
+
+            if (!chestsPoint.Contains(nextPoint))
+            {
+                currentPlayer.PlayerQueue.Enqueue(nextPoint);
+            }
+        }
     }
 
     private static IEnumerable<Point> GetNeighbors(Map map, Point current)
     {
         return _possibleDirections
-            .Select(x => x + current)
+            .Select(point => point + current)
             .Where(point => map.InBounds(point))
             .Where(point => map.Maze[point.X, point.Y] is MapCell.Empty);
     }
 
-    private static List<Point> _possibleDirections = new()
+    private static int CalculateDistance(double x1, double y1, double x2, double y2)
     {
-        new Point(0, -1),
-        new Point(0, 1),
-        new Point(-1, 0),
-        new Point(1, 0),
-    };
+        return (int)(Math.Abs(x2 - x1) + Math.Abs(y2 - y1));
+    }
 }
