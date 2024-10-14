b9cb18371a03091ac914e36842c1412af5c8c274
MrFreuden <mrfreuden@gmail.com>
2024-10-14 14:24:52 +0200

    task done

--- 35.Antiplagiarism/LongestCommonSubsequenceCalculator.cs
+++ 35.Antiplagiarism/LongestCommonSubsequenceCalculator.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Antiplagiarism;
 
 public static class LongestCommonSubsequenceCalculator
-{
-	public static List<string> Calculate(List<string> first, List<string> second)
+{ 
+    public static List<string> Calculate(List<string> first, List<string> second)
 	{
 		var opt = CreateOptimizationTable(first, second);
 		return RestoreAnswer(opt, first, second);
@@ -13,11 +14,42 @@ public static class LongestCommonSubsequenceCalculator
 
 	private static int[,] CreateOptimizationTable(List<string> first, List<string> second)
 	{
-		throw new NotImplementedException();
+        var opt = new int[first.Count + 1, second.Count + 1];
+        for (var i = 1; i <= first.Count; ++i)
+            for (var j = 1; j <= second.Count; ++j)
+            {
+                if (first[i - 1] == second[j - 1])
+                    opt[i, j] = 1 + opt[i - 1, j - 1] ;
+                else
+                    opt[i, j] = Math.Max(opt[i - 1, j], opt[i, j - 1]);
+            }
+
+        return opt;
 	}
 
 	private static List<string> RestoreAnswer(int[,] opt, List<string> first, List<string> second)
 	{
-		throw new NotImplementedException();
-	}
+        var lcs = new Stack<string>();
+        var l1 = first.Count;
+        var l2 = second.Count;
+
+        while (l1 > 0 && l2 > 0)
+        {
+            if (first[l1 - 1] == second[l2 - 1])
+            {
+                lcs.Push(first[l1 - 1]);
+                l1--;
+                l2--;
+            }
+            else
+            {
+                if (opt[l1 - 1, l2] > opt[l1, l2 - 1])
+                    l1--;
+                else
+                    l2--;
+            }
+        }
+
+        return lcs.ToList();
+    }
 }
