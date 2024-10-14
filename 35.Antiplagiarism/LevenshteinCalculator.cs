9bf95c82aec555db0afd0172a86b7ee606275fb4
MrFreuden <mrfreuden@gmail.com>
2024-10-14 12:30:42 +0200

    task done

--- 35.Antiplagiarism/LevenshteinCalculator.cs
+++ 35.Antiplagiarism/LevenshteinCalculator.cs
@@ -1,21 +1,35 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
-// Каждый документ — это список токенов. То есть List<string>.
-// Вместо этого будем использовать псевдоним DocumentTokens.
-// Это поможет избежать сложных конструкций:
-// вместо List<List<string>> будет List<DocumentTokens>
 using DocumentTokens = System.Collections.Generic.List<string>;
 
 namespace Antiplagiarism;
 
 public class LevenshteinCalculator
 {
-	public List<ComparisonResult> CompareDocumentsPairwise(List<DocumentTokens> documents)
-	{
-		return new List<ComparisonResult> {
-			new(
-				documents[0], 
-				documents[1], 
-				TokenDistanceCalculator.GetTokenDistance(documents[0][0], documents[1][0]))};
-	}
+    public List<ComparisonResult> CompareDocumentsPairwise(List<DocumentTokens> documents)
+    {
+        if (documents.Count <= 1) return new List<ComparisonResult>();
+        
+        var documentsPairs = documents.SelectMany((fst, i) => documents.Skip(i + 1).Select(snd => (fst, snd)));
+        var comparisonResults = documentsPairs.Select(x => new ComparisonResult(x.fst, x.snd, LevenshteinDistance(x.fst, x.snd))).ToList();
+
+        return comparisonResults;
+    }
+
+    public static double LevenshteinDistance(DocumentTokens first, DocumentTokens second)
+    {
+        var opt = new double[first.Count + 1, second.Count + 1];
+        for (var i = 0; i <= first.Count; ++i) opt[i, 0] = i;
+        for (var i = 0; i <= second.Count; ++i) opt[0, i] = i;
+
+        for (var i = 1; i <= first.Count; ++i)
+            for (var j = 1; j <= second.Count; ++j)
+            {
+                opt[i, j] = TokenDistanceCalculator.GetTokenDistance(first[i - 1], second[j - 1]);
+                opt[i, j] = Math.Min(opt[i - 1, j] + 1, Math.Min(opt[i, j - 1] + 1, opt[i - 1, j - 1] + opt[i, j]));
+            }
+        return opt[first.Count, second.Count];
+    }
 }
